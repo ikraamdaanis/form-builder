a1593906040b409553a5e9bb32a9ed48d06d1a06
Ikraam <ibndaanis@gmail.com>
2023-11-25 17:41:40 +0000

    removed form component

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,4 +1,3 @@
-import { FormDescription } from "components/ui/form";
 import {
   Tooltip as ShadcnTooltip,
   TooltipContent,
@@ -21,9 +20,7 @@ export const Tooltip = ({ message, children }: Props) => {
       <ShadcnTooltip>
         <TooltipTrigger asChild>{children}</TooltipTrigger>
         <TooltipContent className="max-w-[200px] bg-zinc-50 dark:bg-zinc-900">
-          <FormDescription className="text-zinc-900 dark:text-zinc-50">
-            {message}
-          </FormDescription>
+          <div className="text-zinc-900 dark:text-zinc-50">{message}</div>
         </TooltipContent>
       </ShadcnTooltip>
     </TooltipProvider>
