55b7f65b6d09bb2c1125983a423a6289a6544e7a
Ikraam <ibndaanis@gmail.com>
2023-12-14 14:02:28 +0000

    fetchRecentSubmissions nostore

--- features/submissions/actions/fetchRecentSubmissions.ts
+++ features/submissions/actions/fetchRecentSubmissions.ts
@@ -3,9 +3,12 @@
 import { db } from "database/db";
 import { formSubmissions } from "database/schema";
 import { desc, eq } from "drizzle-orm";
+import { unstable_noStore as noStore } from "next/cache";
 
 /** Fetches the 10 most recent form submissions for a specific form. */
 export async function fetchRecentSubmissions(formId: string) {
+  noStore();
+
   return db.query.formSubmissions.findMany({
     where: eq(formSubmissions.formId, formId),
     limit: 10,
